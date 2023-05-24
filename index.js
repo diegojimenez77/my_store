59667115f09530b3ec46f3059cccedf9bb0b1343
diegojimenez77 <djpcoding8652@gmail.com>
2023-05-24 16:01:52 -0600

    creando el primer servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('my app');
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+})
+
+app.listen(port, () => {
+  console.log('Mi port'+ port);
+});
